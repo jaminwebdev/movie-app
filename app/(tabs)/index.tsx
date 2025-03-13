91dda0aa86fcf41ed66b7f5974d403e29353265d
Jamin <jaminwebdev@gmail.com>
2025-03-12 21:32:55 -0500

    main movie feed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,60 @@
-import { View, Image, ScrollView } from "react-native";
+import React from "react";
+import { View, Image, ScrollView, ActivityIndicator, Text, FlatList } from "react-native";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { getMovies } from "@/services/api";
+import MovieCard from "@/components/MovieCard";
 
 export default function Index() {
 
   const router = useRouter();
+
+  const { 
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError 
+  } = useFetch(() => getMovies({ query: ''}))
+  
+  const maybeRenderMovies = () => {
+    if (moviesLoading) { 
+      return <ActivityIndicator size='large' color='#0000ff' className="mt-10 self-center"/>
+    }
+
+    if (moviesError) {
+      return <Text>Error: {moviesError?.message}</Text>
+    }
+
+    return (
+      <View className="flex-1 mt-5">
+        <SearchBar
+          onPress={() => {
+            router.push("/search");
+          }}
+          placeholder="Search for a movie"
+        />
+        <>
+          <Text className="text-lg text-white font-bold m-5">Latest Movies</Text>
+
+          <FlatList
+              data={movies}
+              renderItem={({ item }) => <MovieCard {...item} />}
+              keyExtractor={(item) => item.id.toString()}
+              numColumns={3}
+              columnWrapperStyle={{
+                justifyContent: "flex-start",
+                gap: 20,
+                marginBottom: 15,
+              }}
+              className="mt-2 pb-32"
+              scrollEnabled={false}
+            />
+        </>
+      </View>
+    )
+  }
 
   return (
     <View className="flex-1 bg-primary">
@@ -20,15 +68,7 @@ export default function Index() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-
-        <View className="flex-1 mt-5">
-          <SearchBar
-            onPress={() => {
-              router.push("/search");
-            }}
-            placeholder="Search for a movie"
-          />
-        </View>
+        {maybeRenderMovies()}
       </ScrollView>
     </View>
   );
