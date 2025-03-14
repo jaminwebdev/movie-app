c179336756ec2d2f091fedd3ef65e2715418038e
Jamin <jaminwebdev@gmail.com>
2025-03-14 16:06:51 -0500

    react query added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import { Stack } from "expo-router";
 import "./globals.css"
 import { StatusBar } from "react-native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <StatusBar hidden={true} />
 
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)"  />
         <Stack.Screen name="movies/[id]"/>
       </Stack>
-    </>
+    </QueryClientProvider>
   ) 
 }
